53c629bc79df78af60e24eca2854cf8919bd876c
Ubuntu <rafacanelongmai@rafacanelongmai.0tvoz3221nweljhpiimhoiqzah.gx.internal.cloudapp.net>
2020-11-26 15:15:32 -0700

    On branch1, I have changed the greet function to have a default value

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,8 @@
-def greet(name, msg):
+def greet(name, msg="Good Morning!"):
 	"""this function greets to 
-	the person with the provided message"""
+	the person with the provided message
+
+	If the message is bot provided,
+	it defaults to 'Good Morning!'"""
+
 	print("Hello", name + ', ' + msg)
